57e1a0964bc2d3461af819fd8168bed4b48d4d81
DannyAgha <danialagha@utilita.co.uk>
2024-07-25 10:48:56 +0100

    Map Composable setup and working showcasing a map view within my application

---
+++ app/src/main/java/com/example/testinggooglemaps/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.testinggooglemaps
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.testinggooglemaps.ui.theme.TestingGoogleMapsTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestingGoogleMapsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MapComposable()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MapComposable(){
+    val defaultCameraPosition = rememberCameraPositionState{
+        position = CameraPosition.fromLatLngZoom(LatLng(50.9161, -1.3649), 12f)
+    }
+
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = defaultCameraPosition
+    )
+}
