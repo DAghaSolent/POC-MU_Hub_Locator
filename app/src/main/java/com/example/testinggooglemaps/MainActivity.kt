6c77d343b81b25afdc90e89a2184cb390e8c23dc
DannyAgha <danialagha@utilita.co.uk>
2024-07-26 19:52:24 +0100

    Custom map marker implemented

--- app/src/main/java/com/example/testinggooglemaps/MainActivity.kt
+++ app/src/main/java/com/example/testinggooglemaps/MainActivity.kt
@@ -1,26 +1,29 @@
 package com.example.testinggooglemaps
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.testinggooglemaps.ui.theme.TestingGoogleMapsTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
+import com.google.maps.android.compose.MarkerComposable
+import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 class MainActivity : ComponentActivity() {
@@ -40,6 +43,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun MapComposable(){
     val defaultCameraPosition = rememberCameraPositionState{
@@ -54,5 +58,15 @@ fun MapComposable(){
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = defaultCameraPosition,
         properties = mapProperties
-    )
+    ){
+        MarkerComposable(
+            state = MarkerState(position = LatLng(50.9161, -1.3649)),
+        ){
+            Image(
+                painterResource(id = R.drawable.utilita),
+                contentDescription = null,
+                Modifier.size(36.dp)
+            )
+        }
+    }
 }
